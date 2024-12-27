d835620d1f6247fefd16d509c7f97abe0f48ab42
unknown <harshitmishra5348@gmail.com>
2024-12-27 16:41:06 +0530

    api update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // const API_URL = 'http://127.0.0.1:8000/api/v1';
-const API_URL = 'https://runskart.boxinallsoftech.com/public/api/v1';
+const API_URL = 'https://paleturquoise-crab-208767.hostingersite.com/public/api/v1';
 
 
 export default API_URL;
